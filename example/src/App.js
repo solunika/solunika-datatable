7f39d1b1132277e9907fe7f4626304dcf65ad92a
Sebastian Perez <Sebastiandperez37@gmail.com>
2022-02-14 13:26:28 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'solunika-datatable'
+import 'solunika-datatable/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
